bb544e6bd5cff3cf2bdcce978438f668d03eeb6a
anamontiaga <amontiaga@gmail.com>
2019-11-19 17:21:10 +0100

    pipes

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { LOCALE_ID, NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+
+import localeEs from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeEs);
+
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+@NgModule({
+  declarations: [AppComponent, CapitalizadoPipe, DomseguroPipe],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: "es" }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
